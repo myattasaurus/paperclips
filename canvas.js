445bd2bf9141cad6e6efd8131fd8feb587bfb4fb
myattasaurus <rcoins84@yahoo.com>
2024-10-14 19:14:07 -0400

    toss paperclip animation

--- canvas.js
+++ canvas.js
@@ -1,6 +1,12 @@
+import { Frame } from "./src/common/Frame.js";
+
 class Paperclip {
+    static #TWO_PI = 2 * Math.PI;
+    static #HALF_PI = Math.PI / 2;
+    static #THREE_HALF_PI = 3 * this.#HALF_PI;
+
     constructor() {
-        this.height = 8
+        this.height = 8;
         this.length = {
             inner: {
                 top: 12,
@@ -13,115 +19,93 @@ class Paperclip {
         };
 
         this.rotation = {
-            perSecond: 1,
+            perSecond: 3,
             radians: 0
         };
 
         this.x = 200;
         this.y = 200;
 
-        this.dx = 0;
-        this.dy = 0;
+        this.dxPerSecond = 400;
+        this.dyPerSecond = -1000;
+        this.ddyPerSecond = 4000;
+
+        this.frame = new Frame((d) => this.#update(d));
     }
 
     update(timestamp) {
-
+        this.frame.tick(timestamp);
     }
-}
 
-let i = 0;
+    #update(duration) {
+        let seconds = duration / 1000;
+        let rotations = this.rotation.perSecond * seconds * Paperclip.#TWO_PI;
+        this.rotation.radians = (this.rotation.radians + rotations) % Paperclip.#TWO_PI;
 
-function onLoad() {
-    requestAnimationFrame(first);
-}
+        this.x += this.dxPerSecond * seconds;
+        this.y += this.dyPerSecond * seconds;
 
-let spawn;
-function first(timestamp) {
-    spawn = timestamp;
-    loop(timestamp);
-    requestAnimationFrame(loop);
-}
+        this.dyPerSecond += this.ddyPerSecond * seconds;
+    }
 
-function loop(timestamp) {
-    let rotationsPerSecond = 1;
-    draw(rotationsPerSecond * timestamp / 1000);
-    requestAnimationFrame(loop);
-}
-
-function draw(secondsSinceSpawn) {
-    let canvas = document.getElementById('canvas');
-    let bodyRect = document.getElementsByTagName('body')[0].getBoundingClientRect();
-    canvas.setAttribute('height', bodyRect.height);
-    canvas.setAttribute('width', bodyRect.width);
-    if (canvas.getContext) {
-        let ctx = canvas.getContext('2d');
-
-        let theta = secondsSinceSpawn % (2 * Math.PI);
+    draw(ctx) {
         ctx.save();
-        ctx.translate(200, 200);
-        ctx.rotate(theta);
-        ctx.translate(-200, -200);
-        draw2();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.rotation.radians);
+        ctx.translate(-this.x, -this.y);
+        this.draw2(ctx);
         ctx.restore();
     }
-}
 
-function draw2() {
-    let canvas = document.getElementById('canvas');
-    let bodyRect = document.getElementsByTagName('body')[0].getBoundingClientRect();
-    // canvas.setAttribute('height', bodyRect.height);
-    // canvas.setAttribute('width', bodyRect.width);
-    if (canvas.getContext) {
-        let ctx = canvas.getContext('2d');
-
+    draw2(ctx) {
         ctx.lineCap = 'round';
         ctx.lineWidth = 1;
         ctx.strokeStyle = 'gray';
 
-        let height = Number(document.getElementById('height').value);
-        let width1 = Number(document.getElementById('width1').value);
-        let width2 = Number(document.getElementById('width2').value);
-        let width3 = Number(document.getElementById('width3').value);
-        let width4 = Number(document.getElementById('width4').value);
-        let tineGap = 0;
+        let height = this.height;
+        let bottomOuterLength = this.length.outer.bottom;
+        let topOuterLength = this.length.outer.top;
+        let bottomInnerLength = this.length.inner.bottom;
+        let topInnerLength = this.length.inner.top;
+
+        let x = this.x;
+        let y = this.y;
 
         ctx.beginPath();
 
-        let x = 200;
-        let y = 200;
-
-        let width = height - 1 + width2;
+        // locate starting point from center (tip of bottom outer length)
+        let width = height - 1 + topOuterLength;
         let radius = height / 2;
-        x += radius + width1 - width / 2;
+        x += radius + bottomOuterLength - width / 2;
         y += height / 2;
         ctx.moveTo(x, y);
 
-        x -= width1;
+        x -= bottomOuterLength;
         ctx.lineTo(x, y);
 
         let centerX = x;
         let centerY = y - radius;
-        ctx.arc(centerX, centerY, radius, Math.PI / 2, 3 * Math.PI / 2, false);
+        ctx.arc(centerX, centerY, radius, Paperclip.#HALF_PI, Paperclip.#THREE_HALF_PI, false);
 
-        x += width2;
+        x += topOuterLength;
         y -= height;
         ctx.lineTo(x, y);
 
         radius -= ctx.lineWidth;
         centerX = x;
         centerY = y + radius;
-        ctx.arc(centerX, centerY, radius, 3 * Math.PI / 2, Math.PI / 2, false);
+        ctx.arc(centerX, centerY, radius, Paperclip.#THREE_HALF_PI, Paperclip.#HALF_PI, false);
 
-        x -= width3;
+        x -= bottomInnerLength;
         y += 2 * radius;
         ctx.lineTo(x, y);
 
         radius -= ctx.lineWidth;
         centerX = x;
         centerY = y - radius;
-        ctx.arc(centerX, centerY, radius, Math.PI / 2, 3 * Math.PI / 2, false);
+        ctx.arc(centerX, centerY, radius, Paperclip.#HALF_PI, Paperclip.#THREE_HALF_PI, false);
 
-        x += width4;
+        x += topInnerLength;
         y -= 2 * radius;
         ctx.lineTo(x, y);
 
@@ -129,9 +113,26 @@ function draw2() {
     }
 }
 
-function drawll() {
-    draw();
-    // draw2();
+let paperclip = new Paperclip();
+onLoad();
+
+function onLoad() {
+    requestAnimationFrame(loop);
+}
+
+function loop(timestamp) {
+    paperclip.update(timestamp);
+
+    let canvas = document.getElementById('canvas');
+    let bodyRect = document.getElementsByTagName('body')[0].getBoundingClientRect();
+    canvas.setAttribute('height', bodyRect.height);
+    canvas.setAttribute('width', bodyRect.width);
+    if (canvas.getContext) {
+        let ctx = canvas.getContext('2d');
+
+        paperclip.draw(ctx);
+    }
+    requestAnimationFrame(loop);
 }
 
 let paperclip8 = {
